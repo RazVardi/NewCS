5638ddab5179e2cf92fbc45bbabae5f50df384d1
RazVardi <45370411+RazVardi@users.noreply.github.com>
2021-01-22 13:59:01 +0200

    Update ProgramStopper.cs

--- ProgramStopper.cs
+++ ProgramStopper.cs
@@ -19,5 +19,38 @@ namespace UdemyStopWatch
             Console.ReadLine();
         }
     }
+    public class StopWatch
+    {   
+        DateTime initialTime;
+        bool b = true;
+        
+        public void Start()
+        {
+            if (b)
+            {
+                initialTime = DateTime.Now;
+                b = false;
+            }
+            else
+            {
+                throw new InvalidOperationException("start  already running");
+            }  
+        }
+        
+        public void Stop()
+        {
+            if (!b)
+            {
+                TimeSpan duration = (DateTime.Now - initialTime);
+                Console.WriteLine(duration);
+                bool b = true;
+            }
+            else
+            {
+                throw new InvalidOperationException("stop already running");
+            }
+           
+        }
+    }
 
 }
